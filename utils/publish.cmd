76b9dcba84e702ff59597e3e9b037df92a5737c3
Mike Minns <mminns@itofinity.co.uk>
2018-12-04 23:39:25 -0800

    WIP

--- utils/publish.cmd
+++ utils/publish.cmd
@@ -1,3 +1,5 @@
 dotnet publish --framework netcoreapp2.1 --runtime win-x64
 dotnet publish --framework netcoreapp2.1 --runtime linux-x64
 dotnet publish --framework netcoreapp2.1 --runtime osx-x64
+
+EXIT /B 0
